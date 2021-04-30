6db66798c3359f00fcbfa17c899a05d0fcb5d228
IreneuszSob <ireksobol10@gmail.com>
2021-04-30 10:28:35 +0200

    LAB8

--- src/App.js
+++ src/App.js
@@ -1,13 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
-import Posts from './components/posts';
+import { Route, Switch } from "react-router-dom";
+import Posts from "./components/posts";
+import Home from "./components/home";
+import NavBar from "./components/common/navbar";
+import NotFound from './components/notFound';
 
 function App() {
-  return (
-    <div className="App">
-      <Posts/>
-    </div>
-  );
+ return (
+     <div className="container-fluid">
+       <NavBar />
+       <div className="container">
+           <div className="content">
+               <Switch>
+                   <Route path="/posts"
+                   render={props => <Posts sortBy="newest" {...props} />}
+                   />
+                   <Route path="/" exact component={Home} />
+                   <Route path="*" exact component={NotFound} />
+               </Switch>
+           </div>
+       </div>
+     </div>
+ );
 }
 
 export default App;
